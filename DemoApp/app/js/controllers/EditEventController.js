f96f1207fb7c830059602494dc4937b0778d6a92
tohidsmart <tohidsmart@gmail.com>
2016-03-17 11:38:10 +0800

    Edit&Validation
    Added Edit New Event
    Validation

---
+++ DemoApp/app/js/controllers/EditEventController.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+eventsApp.controller('EditEventController', function EditEventCOntroller($scope) {
+
+	$scope.saveEvent = function (event,newEventForm) {
+		if(newEventForm.$valid){
+
+		}
+	}
+	$scope.cancelEdit=function(){
+		window.location = "/EventDetails.html";
+	}
+
+});
